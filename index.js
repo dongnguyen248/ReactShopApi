9736fc4fdd04348279824a62488fa4df5b97be38
dongnguyen248 <dacdongdhv@gmail.com>
2021-11-22 15:14:46 +0700

    update

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const mongooes = require("mongoose");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/user");
-const productRoute = require("./routes/product");
-const cartRoute = require("./routes/cart");
-const orderRoute = require("./routes/order");
-var cors = require('cors')
+const express = require('express');
+const mongooes = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/user');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+var cors = require('cors');
 dotenv.config();
 mongooes
-  .connect(process.env.MOGOOSE_URL)
-  .then(() => console.log("Connect DB successfull"))
-  .catch((err) => console.log(err));
+    .connect(process.env.MOGOOSE_URL)
+    .then(() => console.log('Connect DB successfull'))
+    .catch((err) => console.log(err));
 
 const app = express();
 
@@ -20,12 +20,12 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-app.use("/api/users", userRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/products", productRoute);
-app.use("/api/cart", cartRoute);
-app.use("/api/order", orderRoute);
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/order', orderRoute);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Backend");
+    console.log('Backend');
 });
--- routes/order.js
+++ routes/order.js
@@ -1,95 +1,97 @@
-const Order = require("../models/Order");
+const Order = require('../models/Order');
 const {
-  verifyToken,
-  verifyTOkenAndAuthorization,
-  verifyTOkenAndAdmin,
-} = require("./verifyToken");
+    verifyToken,
+    verifyTOkenAndAuthorization,
+    verifyTOkenAndAdmin,
+} = require('./verifyToken');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
 //create
-router.post("/", verifyToken, async (req, res) => {
-  const newOrder = new Order(req.body);
-  try {
-    const saveOrder = await newOrder.save();
-    res.status(200).json(saveOrder);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.post('/', verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body);
+    try {
+        const saveOrder = await newOrder.save();
+        res.status(200).json(saveOrder);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //update user
-router.put("/:id", verifyTOkenAndAdmin, async (req, res) => {
-  try {
-    const updateOrder = await Order.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updateOrder);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.put('/:id', verifyTOkenAndAdmin, async (req, res) => {
+    try {
+        const updateOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true },
+        );
+        res.status(200).json(updateOrder);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // //delete
 
-router.delete("/:id", verifyTOkenAndAuthorization, async (req, res) => {
-  try {
-    Order.findByIdAndDelete(req.params.id);
-    res.status(200).json("Order has been delete...");
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.delete('/:id', verifyTOkenAndAuthorization, async (req, res) => {
+    try {
+        Order.findByIdAndDelete(req.params.id);
+        res.status(200).json('Order has been delete...');
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 //get Order Cart
-router.get("/find/:userId", verifyTOkenAndAuthorization, async (req, res) => {
-  try {
-    console.log(req.params.userId);
-    const orders = await Order.find({ userId: req.params.userId });
-    console.log(orders);
-    res.status(200).json(orders);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/find/:userId', verifyTOkenAndAuthorization, async (req, res) => {
+    try {
+        console.log(req.params.userId);
+        const orders = await Order.find({ userId: req.params.userId });
+        console.log(orders);
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //get all oder
-router.get("/", verifyTOkenAndAdmin, async (req, res) => {
-  try {
-    const orders = Order.find();
-    res.status(200).json(orders);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/', verifyTOkenAndAdmin, async (req, res) => {
+    try {
+        const orders = Order.find();
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 //get Monthy Income
 
-router.get("/imcome", verifyTOkenAndAdmin, async (req, res) => {
-  const date = new Date();
-  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
-  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
-  try {
-    const income = await Order.aggregate([
-      { $match: { createdAt: { $gte: previousMonth } } },
-      {
-        $project: {
-          month: { $month: "$createdAt" },
-          sales: "$amount",
-        },
-      },
-      {
-        $group: {
-          _id: "$month",
-          total: { $sum: "$amount" },
-        },
-      },
-    ]);
-    res.status(200).json(income);
-  } catch (err) {
-    res.status(200).json(err);
-  }
+router.get('/imcome', verifyTOkenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(
+        new Date().setMonth(lastMonth.getMonth() - 1),
+    );
+    try {
+        const income = await Order.aggregate([
+            { $match: { createdAt: { $gte: previousMonth } } },
+            {
+                $project: {
+                    month: { $month: '$createdAt' },
+                    sales: '$amount',
+                },
+            },
+            {
+                $group: {
+                    _id: '$month',
+                    total: { $sum: '$amount' },
+                },
+            },
+        ]);
+        res.status(200).json(income);
+    } catch (err) {
+        res.status(200).json(err);
+    }
 });
 module.exports = router;
